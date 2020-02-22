0cd5af54bfa44d29141d99fe6c3b63756f8db9ef
hassiweb <nryk.hashimoto@gmail.com>
2020-02-22 17:03:18 +0900

    Add a IPv6 pool at PGW for UEs

--- config/run_pgw.sh
+++ config/run_pgw.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 ip tuntap add name ogstun mode tun
+sysctl -w net.ipv6.conf.ogstun.disable_ipv6=0
 ip addr add 45.45.0.1/16 dev ogstun
-#ip addr add cafe::1/16 dev pgwtun
+ip addr add cafe::1/16 dev ogstun
 ip link set ogstun up
 
 # masquerade
@@ -16,4 +17,4 @@ touch /var/log/open5gs/pgw.log
 
 tail -f /var/log/open5gs/pgw.log &
 
-open5gs-pgwd -c  /etc/open5gs/pgw.yaml
+open5gs-pgwd -c /etc/open5gs/pgw.yaml
